e67c2c18af4fb45d9fd2e92aea3cdd0d77cbbc56
Eric Rieman <er28@zips.uakron.edu>
2021-12-23 00:03:59 -0500

    DB conenction string is now built in the code, instead of an environment variable
    It appears variables do not expand across other files.

--- modules/db.js
+++ modules/db.js
@@ -1,6 +1,8 @@
 import { MongoClient } from 'mongodb'
 
-const connection = process.env.DB_CONNECTION
+const connection = process.env.NODE_ENV == 'development' ? 
+                  `mongodb://localhost:27017/${process.env.DB_NAME}` :
+                  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}.y9rq6.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
 export const connectToDb = async () => {
 console.log(connection)
